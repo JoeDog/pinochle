a6975915dbba075915bc9795431c27c85727bb3d
Jeff Fulmer <jeff@joedog.org>
2014-01-19 19:06:24 -0500

    Added uncaught exception handler

--- src/org/joedog/pinochle/game/GameThread.java
+++ src/org/joedog/pinochle/game/GameThread.java
@@ -7,6 +7,12 @@ public class GameThread extends Thread {
 
   public GameThread(Game game) {
     this.game = game;
+    Thread.UncaughtExceptionHandler h = new Thread.UncaughtExceptionHandler() {
+      public void uncaughtException(Thread th, Throwable ex) {
+        System.out.println("Uncaught exception: " + ex);
+      }
+    };
+    this.setUncaughtExceptionHandler(h);
   }
 
   public void run() {
