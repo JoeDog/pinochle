35d6cf6cf997d9b23d0ea0ce2ea772969b76a23c
JoeDog <jeff@joedog.org>
2014-01-24 15:08:58 -0500

    Added to the distribution

---
+++ src/org/joedog/pinochle/Version.java
@@ -0,0 +1,8 @@
+package org.joedog.pinochle;
+
+public final class Version {
+  public final static String version   = "1.0.6";
+  public final static String copyright = "2011-2014";
+  public final static String author    = "Jeffrey Fulmer, et al.";
+  public final static String email     = "jeff@joedog.org";
+}
