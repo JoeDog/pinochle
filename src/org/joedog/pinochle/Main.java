5af1c029691dc1071179db664b4509bc6dbac067
Jeff Fulmer <jeff@joedog.org>
2015-08-16 14:26:50 -0400

    Completely reworked for new architecture

--- src/org/joedog/pinochle/Main.java
+++ src/org/joedog/pinochle/Main.java
@@ -1,140 +1,124 @@
 package org.joedog.pinochle;
 
-import org.joedog.pinochle.control.*;
-import org.joedog.pinochle.model.*;
-import org.joedog.pinochle.game.*;
-import org.joedog.pinochle.player.*;
-import org.joedog.pinochle.view.*;
-import org.joedog.pinochle.view.actions.*;
-
-import javax.swing.JFrame;
-import java.awt.Color;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.Toolkit;
+
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 import java.awt.event.ComponentListener;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentAdapter;
 
-import javax.swing.UIManager;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager.LookAndFeelInfo;
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.WindowConstants;
 
-/**
- * @author Jeffrey Fulmer
- */
-public class Main {
-  private static GameController controller;
-  private static GameActions    actions;
-  private static GameView       view;
-  private static GameModel      model;
-  private static HighScoreModel highs;
-  private static ScoreModel     score;
-  private static GameMenu       menu;
+import org.joedog.pinochle.control.Engine;
+import org.joedog.pinochle.control.Game;
+import org.joedog.pinochle.control.GameThread;
+import org.joedog.pinochle.view.Menu;
+import org.joedog.pinochle.view.View;
+import org.joedog.pinochle.view.actions.GameActions;
 
-  public Main() { }
+import org.joedog.pinochle.game.Card;
+import org.joedog.pinochle.game.Hand;
+import org.joedog.pinochle.game.Pinochle;
 
-  private static void createAndShowGui(GameController controller, GameView view) {
+public class Main extends JPanel implements MouseListener {
+  private static final long serialVersionUID = -2666347118493388423L;
+  private static View      view    = null;
+  private static Engine    engine  = null;
+  private static Game      control = null;
+  private static Menu      menu;
+
+  public Main(View panel) {
+    super(new BorderLayout());
+    panel.addMouseListener(this);
+    this.addMouseListener(this);
+  }
+
+  private static void createAndShowGui(View view, Menu menu) {
     final JFrame frame = new JFrame("Pinochle");
-    GameMenu menu  = new GameMenu(new GameActions(controller));
-    frame.setJMenuBar(menu);
+    int [] point = control.getModelArrayProperty("MainPosition");
+    int [] scope = control.getModelArrayProperty("MainDimension");
+    if (point == null) {
+      point = new int[] {10, 10}; 
+    }
+    if (scope == null) {
+      scope = new int [] {1024, 666};
+    }
+    int dx = (scope[0]  < 1024) ? 1024 : scope[0];
+    int dy = (scope[1]  < 690)  ?  690 : scope[1];
+    JComponent   panel = new Main(view);
+    panel.add(view, BorderLayout.CENTER);
+    view.setFocusable(true);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setContentPane(view.createContentPane());
-    frame.setPreferredSize(new Dimension(970,640));
-    frame.setSize(970, 640);
+    frame.setContentPane(panel);
+    frame.setPreferredSize(new Dimension(dx, dy));
+    frame.setSize(dx, dy);
+    frame.setJMenuBar(menu);
+    frame.pack();
+    frame.setLocation(point[0], point[1]);
     frame.addComponentListener(new ComponentAdapter() {
       public void componentMoved(ComponentEvent e) {
-        //We'll snag and save these properties when we exit
-        System.getProperties().put("main.X", ""+frame.getX());
-        System.getProperties().put("main.Y", ""+frame.getY());
+        control.setModelProperty("MainPosition", "("+frame.getX()+","+frame.getY()+")");
+      }
+      public void componentResized(ComponentEvent e) {
+        control.setModelProperty("MainDimension", "("+frame.getWidth()+","+frame.getHeight()+")");
       }
     });
-    Dimension dim  = Toolkit.getDefaultToolkit().getScreenSize();
-    int x = controller.getIntProperty("MainX");
-    int y = controller.getIntProperty("MainY");
-    if (x == 0 && y == 0) {
-      int w = frame.getSize().width;
-      int h = frame.getSize().height;
-      x = (dim.width-w)/2;
-      y = (dim.height-h)/2;
-    }
-    frame.pack();
-    frame.setLocation(x, y);
-    frame.setVisible(true);
     frame.setVisible(true);
   }
 
-  private static void adjustLAF() throws ClassNotFoundException,
-    InstantiationException, IllegalAccessException,
-    UnsupportedLookAndFeelException {
-    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-      if ("Nimbus".equals(info.getName())) {
-        //UIManager.setLookAndFeel(info.getClassName());
-        break;
-      }
-    }
+  public void mousePressed(MouseEvent e){
+    int x = e.getX();
+    int y = e.getY();
+    //control.select(x, y);
   }
+  public void mouseReleased(MouseEvent e){
+    int x = e.getX();
+    int y = e.getY();
+    control.select(x, y);
+    //control.slide(x, y);
+  }
+  public void mouseClicked(MouseEvent e) {}
+  public void mouseEntered(MouseEvent e) {}
+  public void mouseExited(MouseEvent e) {}
 
-  public static void main(String[] args) {
-    try {
-      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-      adjustLAF();
-    }
-    catch (javax.swing.UnsupportedLookAndFeelException e) {
-      System.err.println("Pinochle requires java-1.6 or higher");
-    }
-    catch (ClassNotFoundException e) {
-      System.err.println("Pinochle requires java-1.6 or higher");
-    }
-    catch (InstantiationException e) {
-    }
-    catch (IllegalAccessException e) {
+  public static void main(String [] args) {
+    if (control == null) {
+      control = new Game();
     }
 
-    Splash splash = new Splash();
-    if (controller == null) {
-      controller = new GameController();
-      splash.setMessage("controller");
-    }
-    if (model == null) {
-      model = new GameModel();
-      splash.setMessage("configuration");
-    }
-    if (score == null) {
-      score = new ScoreModel();
-      splash.setMessage("scoring");
-    }
-    if (highs == null) {
-      highs = new HighScoreModel();
-      splash.setMessage("high scores");
-    }
     if (view == null) {
-      view = new GameView(controller);
-      splash.setMessage("view");
+      view = new View(control);
     }
-    controller.addView(view);
-    controller.addModel(model);
-    controller.addModel(highs);
-    controller.addModel(score);
-    splash.close();
 
-    if (! controller.getHeadless()) {
+    if (menu == null) {
+      menu = new Menu(new GameActions(control));
+    }
+
+    if (engine == null) {
+      engine = new Engine(view);
+    }
+
+    control.setEngine(engine);
+    control.addView(view);
+
+    if (! control.getModelBooleanProperty("Headless")) {
       SwingUtilities.invokeLater(new Runnable() {
         public void run() {
-          createAndShowGui(controller, view);
+          createAndShowGui(view, menu);
         }
       });
     }
 
     while (true) {
-      Game game = new Game(controller, view);
-      GameThread thread = new GameThread(game);
-      controller.addThread(thread);
+      GameThread thread = new GameThread(control);
+      control.addThread(thread);
       thread.start();
       while (thread.isAlive()) ;
     }
-  }
+  } 
 }
-
