1fdd542436c7f80d7827a8442cc0b7c1e46da37b
Jeff Fulmer <jeff@joedog.org>
2015-08-16 14:27:26 -0400

    Version increment: 2.0.0

--- src/org/joedog/pinochle/Version.java
+++ src/org/joedog/pinochle/Version.java
@@ -1,7 +1,7 @@
 package org.joedog.pinochle;
 
 public final class Version {
-  public final static String version   = "1.0.7";
+  public final static String version   = "2.0.0";
   public final static String copyright = "2011-2015";
   public final static String author    = "Jeffrey Fulmer, et al.";
   public final static String email     = "jeff@joedog.org";
