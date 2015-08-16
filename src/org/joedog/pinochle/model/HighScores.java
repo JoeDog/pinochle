cf5dfef21e50fd0242257f6a80030c3fb1f6225f
Jeff Fulmer <jeff@joedog.org>
2015-08-16 14:37:13 -0400

    Reworked for version-2

--- src/org/joedog/pinochle/model/HighScores.java
+++ src/org/joedog/pinochle/model/HighScores.java
@@ -7,14 +7,17 @@ public class HighScores extends ArrayList {
   private final static int LIMIT     = 9;
   static final long serialVersionUID = -3611946473283033478L;
 
+  /**
+   * Constructor - HighScores is a list of single column Score objects
+   */
   public HighScores () {
   }
 
   @Override 
   public boolean add (Object score) {
     if (this.size() >= LIMIT) {
-      int d = ((Score)this.get(this.size()-1)).getScore();
-      if (((Score)score).getScore() < d) {
+      int d = ((Score)this.get(this.size()-1)).getScore(Score.ONE);
+      if (((Score)score).getScore(Score.ONE) < d) {
         return false;
       } else {
         for (int i = LIMIT; i < this.size(); i++) {
@@ -30,9 +33,9 @@ public class HighScores extends ArrayList {
   public boolean isHighScore (int score) {
     if (this.size() < LIMIT) return true;
 
-    int min = ((Score)this.get(0)).getScore();
+    int min = ((Score)this.get(0)).getScore(Score.ONE);
     for (int i = 0; i < this.size(); i ++) {
-      int tmp = ((Score)this.get(i)).getScore();
+      int tmp = ((Score)this.get(i)).getScore(Score.ONE);
       if (tmp < min) {
         min = tmp;
       }
