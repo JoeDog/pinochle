17d95c6d5821be2e913c0c0e5f798e7c863d6f0a
JoeDog <jeff@joedog.org>
2016-03-07 09:12:09 -0500

    Added copyright information.

--- src/org/joedog/pinochle/model/Location.java
+++ src/org/joedog/pinochle/model/Location.java
@@ -1,9 +1,28 @@
 package org.joedog.pinochle.model;
+/**
+ * Copyright (C) 2013-2016
+ * Jeffrey Fulmer - <jeff@joedog.org>, et al.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *--
+ */
 
 import java.util.ArrayList;
 
 /**
- * This class is mostly lifted from JGameGrid
+ * This class borrows heavily from JGameGrid
  * by Aegidius Plüss
  * http://www.aplu.ch/home/apluhomex.jsp?site=45
  */
